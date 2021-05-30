1a51ef35cb8b82c4ac0069cac17ce810e4b0ad0c
Haneenabonser <aabonser@gmail.com>
2021-05-30 12:15:53 +0300

    weather memory

--- weather.js
+++ weather.js
@@ -3,6 +3,7 @@
 const axios = require('axios');
 
 
+let inMemory = {};
 
 // for weather data 
 // http://localhost:3001/weather?city=amman
@@ -11,18 +12,24 @@ function getWeatherData(req, res) {
     let city = req.query.city;
     let key = process.env.WEATHER_API_KEY;
     let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`;
-
-    axios
-    .get(url)
-    .then(result => {
-        let weatherArray = result.data.data.map(item => {
-            return new Weather(item);
-        });
-        res.send(weatherArray);
-    })
-    .catch(error => {
-        res.status(500).send(`Data for this city is not found`);
-    })
+    if (inMemory[city]!== undefined){
+        console.log('from memory');
+        res.send(inMemory[city]);
+    }else{
+        console.log('from API');
+        axios
+        .get(url)
+        .then(result => {
+            let weatherArray = result.data.data.map(item => {
+                return new Weather(item);
+            });
+            inMemory[city]= weatherArray;
+            res.send(weatherArray);
+        })
+        .catch(error => {
+            res.status(500).send(`Data for this city is not found`);
+        })
+    }   
 };
 
 
